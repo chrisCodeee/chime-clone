d9b2ca7c6deeb32360aab5ca3e7f0b3e46fe31d6
Chris <nzeanoruec@gmail.com>
2024-03-14 06:36:26 -0400

    Adjusted padding

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,15 @@ import { NTY, TC, USAToday, WSJ, forbes } from "../../assets";
 import { Carousels, TextContainer } from "../../components";
 
 const Home = () => {
-	const images = [WSJ, forbes, NTY, USAToday, TC];
+	const images = [forbes, NTY, WSJ, USAToday, TC];
 	return (
 		<>
 			<div style={{ maxWidth: "2000px", margin: "auto" }}>
 				<Carousels />
 			</div>
-			<Container className="px-5 px-md-0 py-5 d-flex flex-wrap justify-content-between" style={{ borderBottom: "2px solid rgb(13, 64, 41)" }}>
+			<Container className="px-4 px-md-0 py-5 d-flex flex-wrap justify-content-between" style={{ borderBottom: "2px solid rgb(13, 64, 41)" }}>
 				{images.map((image) => (
-					<div style={{ height: "30px" }} key={image} className="mt-4 me-4">
+					<div style={{ height: "27px" }} key={image} className="mt-4 me-3">
 						<img src={image} alt="Forbes" />
 					</div>
 				))}
